df0763e8919752b9cc1cf670024ed7ed380147d4
Vittorio Carlo <vittoriocarlotoledo@gmail.com>
2021-12-10 18:57:08 -0500

    Agregando cambios en el proyecto

--- JS/app.js
+++ JS/app.js
@@ -41,6 +41,28 @@ let animado = document.querySelectorAll('.animadoScroll');
   window.addEventListener('scroll', scroll);
 
 
+//Efecto texto
+
+let efecto = new Typed('.efectoTexto', {
+  strings: ['<i class="typedEfect">Tecnología</i>',
+            '<i class="typedEfect">Diseño</i>',
+            '<i class="typedEfect">Programación</i>',
+            '<i class="typedEfect">Redes</i>'],
+
+
+  typeSpeed: 45,
+  startDelay: 300,
+  backSpeed: 45,
+  smartBackspace: true,
+  shuffle: true,
+  backDelay: 1500,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: '|',
+  contentType: 'html'
+});
+
 
 
 
